5a69b188e4c0e519ac9b94b672c8f7b118486d96
Clapa Lucian <clapa_lucian@yahoo.com>
2016-04-03 14:53:55 +0300

    some cleanning

---
+++ src/main/java/com/se/controller/MainRestController.java
@@ -0,0 +1,29 @@
+package com.se.controller;
+
+import com.se.model.User;
+import com.se.model.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+
+@Controller("/")
+public class MainRestController {
+
+    @Autowired
+    private UserDAO userDao;
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String home(Model model) {
+        List<User> listUsers = userDao.list();
+        model.addAttribute("employees", listUsers);
+        return "jsonTemplate";
+    }
+
+}
+
