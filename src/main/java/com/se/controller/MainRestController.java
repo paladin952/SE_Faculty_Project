381fae18f522b54e0cff8404582fc45260b1540c
Clapa Lucian <clapa_lucian@yahoo.com>
2016-04-03 14:59:38 +0300

    delete unused POJOS

--- src/main/java/com/se/controller/MainRestController.java
+++ src/main/java/com/se/controller/MainRestController.java
@@ -2,6 +2,7 @@ package com.se.controller;
 
 import com.se.model.User;
 import com.se.model.UserDAO;
+import com.se.util.JsonConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+
 
 
 @Controller("/")
@@ -21,8 +23,8 @@ public class MainRestController {
     @RequestMapping(value = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String home(Model model) {
         List<User> listUsers = userDao.list();
-        model.addAttribute("employees", listUsers);
-        return "jsonTemplate";
+        model.addAttribute(JsonConstants.ROOT_USERS, listUsers);
+        return JsonConstants.JSON_TEMPLATE;
     }
 
 }
---
+++ src/main/java/com/se/util/JsonConstants.java
@@ -0,0 +1,15 @@
+package com.se.util;
+
+/**
+ * Created by Clapa Lucian on 4/3/2016.
+ */
+public class JsonConstants {
+
+    public static final String JSON_TEMPLATE = "jsonTemplate";
+
+    /**
+     * The root string for users json
+     */
+    public static final String ROOT_USERS = "users";
+
+}
