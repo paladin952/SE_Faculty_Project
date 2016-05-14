b1738cb8a32bc94ac5c9a59095343163eaef0315
Clapa Lucian <clapa_lucian@yahoo.com>
2016-05-14 12:10:38 +0300

    add get from scolarship

--- src/main/java/com/se/database/dao/daoImplementation/ScholarshipDAOImpl.java
+++ src/main/java/com/se/database/dao/daoImplementation/ScholarshipDAOImpl.java
@@ -2,16 +2,30 @@ package com.se.database.dao.daoImplementation;
 
 import com.se.database.dao.interfaces.IScholarshipDAO;
 import com.se.database.dao.model.academic.grants.ScholarshipVO;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional
 public class ScholarshipDAOImpl implements IScholarshipDAO {
+    /**
+     * The session factory from spring
+     */
+    private SessionFactory sessionFactory;
+
+    public ScholarshipDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public List<ScholarshipVO> list() {
-        return null;
+        @SuppressWarnings("unchecked")
+        List<ScholarshipVO> listUser = (List<ScholarshipVO>) sessionFactory.getCurrentSession()
+                .createCriteria(ScholarshipVO.class)
+                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+        return listUser;
     }
 
     @Override
--- src/main/java/com/se/database/dao/model/academic/grants/ScholarshipVO.java
+++ src/main/java/com/se/database/dao/model/academic/grants/ScholarshipVO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 
 @Entity
-@Table(name="scholarhip")
+@Table(name="scholarship")
 public class ScholarshipVO implements Serializable {
 
     @Id
