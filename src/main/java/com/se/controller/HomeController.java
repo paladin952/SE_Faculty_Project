0abbf9800fe25d288444daedc90a102cc1ce6e59
Clapa Lucian <clapa_lucian@yahoo.com>
2016-05-17 21:15:45 +0300

    update spring + add crossorigin

--- src/main/java/com/se/controller/HomeController.java
+++ src/main/java/com/se/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.se.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller("/")
+@CrossOrigin
 public class HomeController {
 
     @RequestMapping("/")
